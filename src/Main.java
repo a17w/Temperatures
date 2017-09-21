371f5fdea51ad3bc019658d85809af5b5e80f256
Annie Wong <annie.hs.wong@gmail.com>
2017-09-21 10:55:31 -0700

    reorganized code using method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
+        // create object of Temp class
+        Temp user = new Temp();
+
+        // then call promptUserTemp method
+        user.promptUserTemp();
+
+        /*
 
         // declare variables
         double tempf;
@@ -15,7 +25,27 @@ public class Main {
         tempc = (tempf -32) * 5.0/9.0; // Carry out the math operations to convert the temperature from Fahrenheit to Celsius.
         System.out.println("Temp in Celcius is: " + tempc); // Output the temperature in Celsius.
 
+        */
     }
+
+}
+
+
+class Temp
+{
+    // declared variables inside the Temp class
+    private double tempf;
+    private double tempc;
+
+    public void promptUserTemp()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a temp in Fahrenheit: ");
+        tempf = Double.parseDouble(input.next());
+        tempc = (tempf -32) * 5.0/9.0;
+        System.out.println("Temp in Celcius is: " + tempc);
+    }
+
 
 }
 
